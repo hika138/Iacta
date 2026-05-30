fd56962c20529bae00c10766b2194b98b2ecae26
Hika <46370237+hika138@users.noreply.github.com>
2026-05-30 13:02:29 +0900

    fix: トークナイザーの演算子正規表現を拡張して大文字演算子をサポート (#8)
    * fix: 二項演算子の処理を拡張して新しい演算子をサポート
    
    * fix: トークナイザーで演算子の正規表現を拡張して大文字の演算子をサポート

--- modules/myast.py
+++ modules/myast.py
@@ -32,11 +32,11 @@ class BinOp(Node):
             return left + right
         elif self.op == '-':
             return left - right
-        elif self.op == '*':
+        elif self.op == '*' or self.op == '×':
             return left * right
-        elif self.op == '/':
+        elif self.op == '/' or self.op == '÷':
             return left / right
-        elif self.op == 'd':
+        elif self.op == 'd' or self.op == 'D':
             total = 0
             for _ in range(int(left)):
                 total += random.randint(1, int(right))
@@ -77,7 +77,7 @@ class Tokenizer:
         self.current_char: str = self.text[self.pos] if self.text else None
         self.token_specification = [
             (T_NUM,      r'\d+(\.\d*)?'),   # Integer or decimal number
-            (T_OP,       r'[+\-*/d]'),      # Arithmetic operators
+            (T_OP,       r'[+\-*/dD×÷]'),      # Arithmetic operators
             (T_LPAREN,   r'\('),             # Left Parenthesis
             (T_RPAREN,   r'\)'),             # Right Parenthesis
             (T_SKIP,     r'[ \s]+'),         # Skip over spaces and tabs
@@ -147,7 +147,7 @@ class Parser:
 
     def _term(self) -> Node:
         node = self._factor()
-        while self.current_token.type == T_OP and self.current_token.value in ('*', '/', 'd'):
+        while self.current_token.type == T_OP and self.current_token.value in ('*', '×', '/', '÷', 'd', 'D'):
             token = self.current_token
             self._eat(T_OP)
             node = BinOp(left_child=node, op=token.value, right_child=self._factor())
